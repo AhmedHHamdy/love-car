4573f7a3e0d3580c0b7d9beee590df26881f4190
Ahmed Hamdy <82416131+AhmedHHamdy@users.noreply.github.com>
2024-01-28 14:29:16 +0200

    Change message show time

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -97,7 +97,7 @@ export default function Header() {
 
             setTimeout(() => {
               setShowNotification(false)
-            }, 8000)
+            }, 300000)
 
             requestNotificationPermission(data.notifications); 
 
@@ -125,7 +125,7 @@ export default function Header() {
 
     const pollingInterval = setInterval(() => {
       loadData()
-    }, 5000)
+    }, 15000)
 
     return () => clearInterval(pollingInterval)
 
