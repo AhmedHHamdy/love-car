cff281318eb7e51df795159ff3ec58b1fc67ed2a
ahmedhhamdy <ahmedhhamdyad@gmail.com>
2025-08-18 22:59:00 +0300

    feat: update libraries

--- src/pages/account/Login.jsx
+++ src/pages/account/Login.jsx
@@ -72,15 +72,7 @@ export default function Login() {
     
 
     function handleShowPassword() {
-      const passwordInput = document.getElementById("password") 
-      if (passwordInput) {
-        if (passwordInput.type === "text") {
-          passwordInput.type = "password";
-        } else {
-          passwordInput.type = "text";
-        }
-        setShowPassword((previousValue) => !previousValue);
-      }
+      setShowPassword((previousValue) => !previousValue);
     }
 
 
@@ -128,7 +120,7 @@ export default function Login() {
                 <label className="input bg-secondary input-bordered flex items-center relative gap-2 col-span-2">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
                   <input
-                    type="password"
+                    type={showPassword ? "text" : "password"}
                     name="password"
                     id="password"
                     placeholder="••••••••"
--- src/pages/account/Signup.jsx
+++ src/pages/account/Signup.jsx
@@ -159,28 +159,12 @@ export default function Signup() {
     }
 
     function handleShowPassword() {
-      const passwordInput = document.getElementById("password") 
-      if (passwordInput) {
-        if (passwordInput.type === "text") {
-          passwordInput.type = "password";
-        } else {
-          passwordInput.type = "text";
-        }
-        setShowPassword((previousValue) => !previousValue);
-      }
+      setShowPassword((previousValue) => !previousValue);
     }
 
     
     function handleShowConfirmPassword() {
-      const passwordInput = document.getElementById("confirm_password") 
-      if (passwordInput) {
-        if (passwordInput.type === "text") {
-          passwordInput.type = "password";
-        } else {
-          passwordInput.type = "text";
-        }
-        setShowConfirmPassword((previousValue) => !previousValue);
-      }
+      setShowConfirmPassword((previousValue) => !previousValue);
     }
 
 
@@ -392,7 +376,7 @@ export default function Signup() {
                 <label className="input bg-secondary relative input-bordered flex items-center gap-2 col-span-2">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
                   <input
-                    type="password"
+                    type={showPassword ? "text" : "password"}
                     name="password"
                     id="password"
                     placeholder={t("Password")}
@@ -417,7 +401,7 @@ export default function Signup() {
                 <label className="input bg-secondary relative input-bordered flex items-center gap-2 col-span-2">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
                   <input
-                    type="password"
+                    type={showConfirmPassword ? "text" : "password"}
                     name="confirm_password"
                     id="confirm_password"
                     placeholder={t("Confirm Password")}
